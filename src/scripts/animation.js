649c17d91993d3935cde697769a404c4712dda1a
Nathan-rs <nata.ramos@acad.ifma.edu.br>
2024-10-28 08:52:13 -0300

    created function in object animation e created file main.js

---
+++ src/scripts/animation.js
@@ -0,0 +1,19 @@
+
+class Animation {
+
+    constructor(ctx) {
+        this.sprites = []
+        this.ligado = false
+        this.context = ctx
+    }
+
+    novoSprite() { }
+
+    ligar() { }
+
+    desligar() { }
+
+    limparTela() { }
+
+    proximoFrame() { }
+}
